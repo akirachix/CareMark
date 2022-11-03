3bc6d865a781f4bece73712436cb614b0e198c4a
Akuot-philip <akuotphilipp@gmail.com>
2022-11-03 09:46:20 +0300

    timed the congratulations screen

--- app/src/main/java/com/example/caremark/ui/CongratulationsActivity.kt
+++ app/src/main/java/com/example/caremark/ui/CongratulationsActivity.kt
@@ -1,21 +1,33 @@
 package com.example.caremark.ui
 
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.viewModels
 import com.example.caremark.R
 import com.example.caremark.ViewModel.UserViewModel
 import com.example.caremark.databinding.ActivityCongratulationsBinding
+import com.example.caremark.databinding.ActivityForgotpasswordBinding
 import com.example.caremark.databinding.ActivityLoginBinding
+import com.example.caremark.utils.Constants.Companion.accessToken
 
 class CongratulationsActivity : AppCompatActivity() {
     lateinit var binding: ActivityCongratulationsBinding
+//    lateinit var sharedPrefs: SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =ActivityCongratulationsBinding.inflate(layoutInflater)
+        binding = ActivityCongratulationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val handler= Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        },3000)
 
     }
 }
